ac3d47be65ae26274e9cb06e607aa9a6acd8fa56
zohaibsaeed-learner <saeedzohaib677@gmail.com>
2026-04-26 01:41:18 +0500

    Initial commit: Landing page with navbar

---
+++ Task1.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
